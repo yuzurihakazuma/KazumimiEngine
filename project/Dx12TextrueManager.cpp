56753dd3c31a07cbd418183d51432602051b2017
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-11-15 17:09:21 +0900

    s

---
+++ project/Dx12TextrueManager.cpp
@@ -0,0 +1,1 @@
+#include "Dx12TextrueManager.h"
---
+++ project/Dx12TextrueManager.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <wrl.h>
+#include "externals/DirectXTex/d3dx12.h"
+#include "externals/DirectXTex/DirectXTex.h"
+class Dx12TextrueManager{
+
+
+
+
+};
+
